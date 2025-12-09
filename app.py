25c6d5d31eac64ab0564058230793c4c11452640
Akshat Bansal <akshatb004@gmail.com>
2025-12-09 10:38:55 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    try:
+        db = mysql.connector.connect(
+            host="mysql",          # container name of MySQL
+            user="root",
+            password="root",
+            database="testdb"
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT 'Hello from MySQL!'")
+        result = cursor.fetchone()
+        return result[0]
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6000)
